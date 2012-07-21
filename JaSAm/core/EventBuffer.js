b943979402a316fdb83c34bd0c8b6fe5f718141e
luka5 <github@luka5.de>
2012-07-21 17:43:21 +0200

    bugfix memory leak

--- JaSAm/core/EventBuffer.js
+++ JaSAm/core/EventBuffer.js
@@ -9,16 +9,17 @@ var EventBuffer = function(){
     
     this.next = function(lasttime){
         var now = (new Date()).getTime();
+        var next = null;
         for(var timestamp in store){
-            if(timestamp > lasttime){
+            if(timestamp > lasttime && next == null){
                 //return next store entry
-                return store[timestamp];
+                next = store[timestamp];
             }else if(timestamp < now-2000){
                 //delete old store entries
                 delete store[timestamp];
             }
         }
-        return null;
+        return next;
     };
     
 };
